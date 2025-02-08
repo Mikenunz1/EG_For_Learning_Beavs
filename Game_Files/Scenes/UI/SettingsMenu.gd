db9666972b9a027f31539ef36a27da25858db504
Mikenunz1 <mikenunzioo@gmail.com>
2025-02-08 15:24:33 -0800

    Adding Nonfunctional Text-size dropdown
    Adding a drop down button to the main settings menu which includes a placeholder function for when the text size can actually be changed

--- Game_Files/Scenes/UI/SettingsMenu.gd
+++ Game_Files/Scenes/UI/SettingsMenu.gd
@@ -6,3 +6,14 @@ func setGameResolution (val):
 			DisplayServer.window_set_size(Vector2i(1920, 1080))
 		1:
 			DisplayServer.window_set_size(Vector2i(1280, 720))
+
+
+func setTextSize (val):
+	match val:
+		0: 
+			pass
+		1:
+			pass
+		2:
+			pass	
+		
