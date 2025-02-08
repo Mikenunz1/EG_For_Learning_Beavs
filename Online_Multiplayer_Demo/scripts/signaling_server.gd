33269465d1c4c0369d8300aca9fc2604d1e408eb
guimoralesCC <146776753+guimoralesCC@users.noreply.github.com>
2025-02-07 19:59:49 -0800

    Update signaling_server.gd
    Added more debugging fuctions
    Added a real signaling server address

--- Online_Multiplayer_Demo/scripts/signaling_server.gd
+++ Online_Multiplayer_Demo/scripts/signaling_server.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name ConnectionHandler
 
 signal connection_failed
 signal player_joined(peer_id: int)
@@ -8,58 +9,77 @@ var is_listening := false
 var current_room := ""
 
 func _ready():
-	# Set up WebSocket signals
-	ws_client.connection_established.connect(_on_connection_established)
-	ws_client.connection_failed.connect(_on_connection_failed)
-	ws_client.data_received.connect(_on_data_received)
+	# Set up WebSocket connection with the Render.com server
+	var err = ws_client.connect_to_url("wss://signaling-server-diku.onrender.com")
+	if err != OK:
+		print("WebSocket connection failed with error: ", err)
+		emit_signal("connection_failed")
 
 func start_listening(room_code: String):
 	current_room = room_code
 	is_listening = true
-	_connect_to_signaling_server()
+	print("Starting to listen on room: ", room_code)
+	_send_message({
+		"type": "join",
+		"room": current_room,
+		"is_host": true
+	})
 
 func join_room(room_code: String):
 	current_room = room_code
 	is_listening = false
-	_connect_to_signaling_server()
-
-func _connect_to_signaling_server():
-	var error = ws_client.connect_to_url("wss://your-signaling-server.com")
-	if error != OK:
-		emit_signal("connection_failed")
-
-func _on_connection_established():
-	var msg = {
+	print("Joining room: ", room_code)
+	_send_message({
 		"type": "join",
 		"room": current_room,
-		"is_host": is_listening
-	}
-	_send_message(msg)
-
-func _on_connection_failed():
-	emit_signal("connection_failed")
-
-func _on_data_received():
-	var data = ws_client.get_packet().get_string_from_utf8()
-	var msg = JSON.parse_string(data)
-	
-	match msg.type:
-		"offer":
-			get_parent()._on_connection_data(msg.peer_id, {
-				"offer": msg.offer,
-				"ice_candidates": msg.candidates
-			})
-		"answer":
-			get_parent()._on_connection_data(msg.peer_id, {
-				"answer": msg.answer,
-				"ice_candidates": msg.candidates
-			})
-		"player_joined":
-			emit_signal("player_joined", msg.peer_id)
+		"is_host": false
+	})
 
 func _send_message(msg: Dictionary):
 	var data = JSON.stringify(msg)
-	ws_client.send(data.to_utf8_buffer())
+	print("Sending message: ", data)
+	ws_client.send_text(data)
 
 func _process(_delta):
 	ws_client.poll()
+	
+	var state = ws_client.get_ready_state()
+	if state == WebSocketPeer.STATE_OPEN:
+		while ws_client.get_available_packet_count():
+			var packet = ws_client.get_packet()
+			var text = packet.get_string_from_utf8()
+			print("Received packet: ", text)
+			var json = JSON.parse_string(text)
+			if json:
+				_handle_message(json)
+	elif state == WebSocketPeer.STATE_CLOSED:
+		print("WebSocket closed with code: ", ws_client.get_close_code())
+		emit_signal("connection_failed")
+
+func _handle_message(msg: Dictionary):
+	print("Handling message: ", msg)
+	
+	if not "type" in msg:
+		print("Message has no type field")
+		return
+		
+	match msg.type:
+		"offer":
+			if "id" in msg:
+				get_parent()._on_connection_data(msg.id, {
+					"offer": msg.offer,
+					"ice_candidates": msg.get("candidates", [])
+				})
+		"answer":
+			if "id" in msg:
+				get_parent()._on_connection_data(msg.id, {
+					"answer": msg.answer,
+					"ice_candidates": msg.get("candidates", [])
+				})
+		"player_joined":
+			if "id" in msg:
+				print("Player joined with id: ", msg.id)
+				emit_signal("player_joined", msg.id)
+		"peer-id":
+			if "id" in msg:
+				print("Received peer ID: ", msg.id)
