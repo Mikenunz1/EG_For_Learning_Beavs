c696d7b96609734c89322d0037b5fb42ca79c4a6
Mikenunz1 <mikenunzioo@gmail.com>
2025-02-25 00:06:17 -0800

    Backgrounds and UI
    Added some basic background elements and UI elements to the game

--- Game_Files/Scripts/MinigameSwimming.gd
+++ Game_Files/Scripts/MinigameSwimming.gd
@@ -7,6 +7,8 @@ var test = 0
 var max = 150
 var min = 40
 
+var spawn_offset = -300
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,21 +19,26 @@ func _process(delta):
 	test += 1
 	
 	if (test == 30):
-		spawn_Log(-300, 400)
+		spawn_Log(400)
 	
 	if (test == max):
-		spawn_Rock(-300, 800)
+		spawn_Rock(800)
 		test = 0
 		if (max >= min):
 			max = max - 10
 	
 #The below functions spawn specifc obstacles for the minigame
-func spawn_Rock(xpos, ypos):
+func spawn_Rock(ypos):
 	var newRock = rock.instantiate()
 	get_tree().current_scene.add_child(newRock)
-	newRock.global_position = Vector2(xpos, ypos)
+	newRock.global_position = Vector2(spawn_offset, ypos)
 	
-func spawn_Log(xpos, ypos):
+func spawn_Log(ypos):
 	var newLog = log.instantiate()
 	get_tree().current_scene.add_child(newLog)
-	newLog.global_position = Vector2(xpos, ypos)
+	newLog.global_position = Vector2(spawn_offset, ypos)
+	
+#TODO
+#spawn log long
+#spawn tree root
+#spawn salmon
