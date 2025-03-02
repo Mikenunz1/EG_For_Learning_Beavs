b60c42e1ecf59ead0333796d8bb248d27f31918d
pasquang <pasquang@oregonstate.edu>
2025-03-02 15:20:47 -0800

    Added Menu SFX + bugfix
    Additional bugfix: added a setting open = false to the hideMenu() in PauseMenu.gd so that resume button would set open to false.  Otherwise, pressing resume would require two presses of escape to bring up to pause menu.

--- Game_Files/Scripts/UI/PauseMenu.gd
+++ Game_Files/Scripts/UI/PauseMenu.gd
@@ -4,6 +4,14 @@ extends Control
 @onready var settingsButton = $SettingsText/SettingsButton
 @onready var quitButton = $QuitText/QuitButton
 
+#Variables to hold AudioStreamPlayers
+@onready var menuAudioStream: = $"AudioStreamPlayer-PauseMenuSFX"
+
+#Variables to hold imported audio files for menu sfx
+@onready var blip = load("res://Game_Files/Assets/Audio/Sounds/UI SFX/menu-sfx2.mp3")
+@onready var select = load("res://Game_Files/Assets/Audio/Sounds/UI SFX/menu-sfx3.mp3")
+@onready var exit = load("res://Game_Files/Assets/Audio/Sounds/UI SFX/menu-sfx.mp3")
+
 var pause = false
 var open = false
 
@@ -19,18 +27,27 @@ func _process(_delta):
 		open = false
 		hideMenu()
 
+func playSound(file):
+	menuAudioStream.stream = (file)
+	menuAudioStream.play()
+
 func openMenu():
+	playSound(blip)
 	show()
 	resumeButton.disabled = false
 	settingsButton.disabled = false
 	quitButton.disabled = false
 	
 func hideMenu():
+	playSound(blip)
 	hide()
+	open = false
 	resumeButton.disabled = true
 	settingsButton.disabled = true
 	quitButton.disabled = true
 	
 func quitPressed():
+	playSound(exit)
+	await get_tree().create_timer(0.2).timeout
 	get_tree().call_group("GameManager", "saveGame")
 	get_tree().call_group("GameManager", "loadSceneByName", "MainMenu")
