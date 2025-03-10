3b1ce7c385855e2dffae98b7e00b1c2f529703cb
pasquang <pasquang@oregonstate.edu>
2025-03-09 18:43:32 -0700

    Added player movement tester
    Possible that the @onready variables will cause errors with godot tester

---
+++ test/unit/test_player_movement_audio.gd
@@ -0,0 +1,34 @@
+extends GutTest
+
+var Level = load('res://Game_Files/Scenes/Functional/GameManager.tscn')
+
+var Player = load("res://Game_Files/Scenes/Player/PlayerInGame.tscn")
+var GameManager = load("res://Game_Files/Scenes/Functional/GameManager.tscn")
+
+var p = null
+var g = null
+var _sender = InputSender.new(Input)
+
+#this function runs before each test function
+#it sets up prerequisite assumptions for each test
+func before_each():
+	#create and add an instance of player and game manager to scene tree
+	p = Player.instantiate()
+	g = GameManager.instantiate()
+	add_child_autofree(p)
+	add_child_autofree(g)
+	wait_frames(1,"WAIT")
+
+#this function runs after each test
+#it cleans up the input sender
+func after_each():
+	_sender.release_all()
+	_sender.clear()
+
+#test player movement to the right
+func test_move_right():
+	_sender.action_down("Move_Right").hold_for(1) #hold down the action for 1 sec
+	simulate(p, 1.0, 10) #simulate that action on the player
+	_sender.action_up("Move_Right") #release the action
+	p.sendToManager() #send positional update to game manager
+	assert_eq(p.playerWalkingAudioStream.playing, true) 
